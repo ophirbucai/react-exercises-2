d93af77cc75031dbc9d8f66e42ccbc6f8cf738e1
Ophir Bucai <ophirbucai@gmail.com>
2021-08-17 22:18:55 +0300

    Removed global flag as it intefered w/ replaceAll method

--- src/Marker/Item/Item.js
+++ src/Marker/Item/Item.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 function Item({item, searchTerm}) {
     const [markedItem, setMarkedItem] = useState();
     useEffect(() => {
-        const matches = item.match(new RegExp(searchTerm, 'gi'));
+        const matches = item.match(new RegExp(searchTerm, 'i'));
         setMarkedItem(item.replaceAll(matches, "<mark>" + matches + "</mark>"));
     }, [searchTerm]);
 
